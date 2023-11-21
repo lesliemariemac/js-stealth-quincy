d474424cd8d9af3bd11292fb34fc8fa7fa544817
Leslie M <lesliemariemac@gmail.com>
2023-11-20 20:55:31 -0500

    Added if else for toggle and h1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 var fans = document.querySelector(".fans");
 var fish = document.querySelector(".fish");
 var pets = document.querySelector(".pets");
+var themeSwitchInput = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var h1 = document.querySelector("h1");
+var body = document.querySelector("body");
+
 
 fans.addEventListener("mouseover", function () {
     fans.innerText = "123K";
@@ -10,4 +15,16 @@ fish.addEventListener("mouseover", function () {
 });
 pets.addEventListener("mouseover", function () {
     pets.innerText = "20K";
+});
+
+themeSwitchInput.addEventListener("click", function () {
+    if (body.classList.contains("light")) {
+        body.classList.remove("light");
+        ball.classList.remove("move-right");
+        h1.innerText = "Stealth Quincy";
+    } else {
+        body.classList.add("light");
+        ball.classList.add("move-right");
+        h1.innerText ="Party Quincy";
+    }
 });
